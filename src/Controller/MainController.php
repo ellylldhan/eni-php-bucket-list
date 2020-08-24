c30bb7956189328416be1e062cd0013cb35f2f3e
ellylldhan <rinux.rinux@gmail.com>
2020-08-25 01:35:51 +0200

    2020-08-25 0135 : mod3 tp1 premiere page

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController {
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+        $module = 3;
+        $tp = 1;
+        $branch      = "Mod3-Twig";
+        $description = "Module 3 - Setup de Twig";
+
+        return $this->render('main/home.html.twig', [
+            "version"     => $module . '.' . $tp,
+            "branch"      => $branch,
+            "description" => $description]);
+    }
+}
